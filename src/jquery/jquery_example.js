65e4d3aa426f6cefe2e7cf0903e3d67f47a60077
송 종근 <carrotcarrot@Baepeu.local>
2019-03-14 13:08:43 +0900

    jQuery

---
+++ src/jquery/jquery_example.js
@@ -0,0 +1,14 @@
+/*
+    웹 브라우저가 문서를 표시하는 순서
+    1. HTML 로드
+    2. DOM 생성
+    3. $(document).ready() 실행
+    4. 이미지 등 요소 로드
+    5. 페이지 로딩 완료
+    6. $(window).ready() // load는 3.0에서 removed, 1.8 deprecated
+
+    Event
+    Form : blur, change
+    Mouse : click, contextmenu, dblclick, mousedown, mouseenter, mouseleave, mousemove, mouseout, mouseover, mouseup
+    Keyboard : keydown, keypress, keyup
+*/
